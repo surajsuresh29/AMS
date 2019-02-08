4768f3769f34ea54f944d624b2a5d3d3fb9d0de0
unni123 <unnikrishnanps26@gmail.com>
2019-02-08 15:43:56 +0530

    Login UI Update

--- app/src/main/java/com/bosscorp/ams/StudentLogin.java
+++ app/src/main/java/com/bosscorp/ams/StudentLogin.java
@@ -3,8 +3,13 @@ package com.bosscorp.ams;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,69 +27,82 @@ import java.util.Map;
 
 public class StudentLogin extends AppCompatActivity {
 
-    private static final String NAME_KEY = "Name";
-    private static final String EMAIL_KEY = "Email";
-    private static final String PHONE_KEY = "Phone";
     FirebaseFirestore db;
-    TextView textDisplay;
+    EditText textDisplay1, textDisplay2;
+    Button login;
+    String course;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
         db = FirebaseFirestore.getInstance();
-        textDisplay = findViewById(R.id.textView2);
+        textDisplay1 = findViewById(R.id.username);
+        textDisplay2 = findViewById(R.id.password);
+        login = findViewById(R.id.login);
         Spinner dropdown = findViewById(R.id.course);
         String[] items = new String[]{"Int MCA 2015", "Int MCA 2016", "Int MCA 2017", "Int MCA 2018", "MCA LAT 2017", "MCA LAT 2018"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
-        //addNewContact();
-        //ReadSingleContact();
-    }
-
-    private void addNewContact() {
-        Map<String, Object> newContact = new HashMap<>();
-        newContact.put(NAME_KEY, "John");
-        newContact.put(EMAIL_KEY, "john@gmail.com");
-        newContact.put(PHONE_KEY, "080-0808-009");
-        db.collection("PhoneBook").document("Contacts").set(newContact)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(StudentLogin.this, "User Registered",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(StudentLogin.this, "ERROR" + e.toString(),
-                                Toast.LENGTH_SHORT).show();
-                        Log.d("TAG", e.toString());
-                    }
-                });
-
-    }
-
-    private void ReadSingleContact() {
-        DocumentReference user = db.collection("PhoneBook").document("Contacts");
-        user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onComplete(@NonNull Task< DocumentSnapshot > task) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                course = (String) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(textDisplay1.getText()))
+                {
+                    textDisplay1.setError("Please enter your Register No.");
+                }
+                if(TextUtils.isEmpty(textDisplay2.getText()))
+                {
+                    textDisplay2.setError("Please enter your Password.");
+                }
+                else{
+                    Login(v);
+                }
+
+            }
+        });
+    }
+
+
+    public void Login(View view) {
+        DocumentReference docRef = db.collection("courses").document(String.valueOf(course))
+                .collection("students").document(String.valueOf(textDisplay1));
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
-                    DocumentSnapshot doc = task.getResult();
-                    StringBuilder fields = new StringBuilder("");
-                    fields.append("Name: ").append(doc.get("Name"));
-                    fields.append("\nEmail: ").append(doc.get("Email"));
-                    fields.append("\nPhone: ").append(doc.get("Phone"));
-                    textDisplay.setText(fields.toString());
+                    DocumentSnapshot document = task.getResult();
+                    if (document != null && document.exists()) {
+                        String pass = String.valueOf(document.get("password"));
+                        if(pass.equals(String.valueOf(textDisplay2.getText())))
+                        {
+                            Toast.makeText(StudentLogin.this, "Successfully logged in.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    else {
+                        Toast.makeText(StudentLogin.this, "No document found.",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+                else {
+                    Toast.makeText(StudentLogin.this, "Document fetching failed.",
+                            Toast.LENGTH_LONG).show();
                 }
             }
-        })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                    }
-                });
+        });
     }
+
 }
