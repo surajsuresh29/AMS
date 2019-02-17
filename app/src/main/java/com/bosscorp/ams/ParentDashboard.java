02ef359a7dc16ca5c9ee56a4797cdc3efa88bef0
SURAJ SURESH <surajcheboy29@gmail.com>
2019-02-17 16:11:36 +0530

    UI UPDATE 17/02/19

---
+++ app/src/main/java/com/bosscorp/ams/ParentDashboard.java
@@ -0,0 +1,13 @@
+package com.bosscorp.ams;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParentDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_dashboard);
+    }
+}
---
+++ app/src/main/java/com/bosscorp/ams/ParentLogin.java
@@ -0,0 +1,109 @@
+package com.bosscorp.ams;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;;import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class ParentLogin extends AppCompatActivity {
+
+    FirebaseFirestore db;
+    EditText username, password;
+    Button login;
+    String course;
+    //String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_login);
+        db = FirebaseFirestore.getInstance();
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        Spinner dropdown = findViewById(R.id.course);
+        String[] items = new String[]{"Int MCA 2015", "Int MCA 2016", "Int MCA 2017", "Int MCA 2018", "MCA LAT 2017", "MCA LAT 2018"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                course = (String) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(username.getText()))
+                {
+                    username.setError("Please enter your Register No.");
+                }
+                if(TextUtils.isEmpty(password.getText()))
+                {
+                    password.setError("Please enter your Password.");
+                }
+                else{
+                    Login(v);
+                }
+
+            }
+        });
+    }
+
+
+
+    public void Login(View view) {
+        DocumentReference docRef = db.collection("courses").document(String.valueOf(course))
+                .collection("students").document(String.valueOf(username.getText()));
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document != null && document.exists()) {
+                        String pass = String.valueOf(document.get("password"));
+                        if(pass.equals(String.valueOf(password.getText())))
+
+                        {
+                            Toast.makeText(ParentLogin.this, "Successfully logged in.",
+                                    Toast.LENGTH_LONG).show();
+                            Intent dash = new Intent(getApplicationContext(), StudentDashboard.class);
+                            startActivity(dash);
+                        }
+                    }
+                    else {
+                        Toast.makeText(ParentLogin.this, "Please check your login credentials.",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+                else {
+                    Toast.makeText(ParentLogin.this, "Document fetching failed.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+}
